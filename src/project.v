3f56c9738684ae19edaa385eaaa5fe53f4239081
oakame <reuellpandher@gmail.com>
2025-12-09 18:50:06 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -4,24 +4,37 @@
  */
 
 `default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_reuel_pandher_nand (
+input wire [7:0] ui_in, // Dedicated inputs
+output wire [7:0] uo_out, // Dedicated outputs
+input wire [7:0] uio_in, // IOs: Input path
+output wire [7:0] uio_out, // IOs: Output path
+output wire [7:0] uio_oe, // IOs: Enable path (active high:
+0=input, 1=output)
+input wire ena, // always 1 when the design is powered,
+so you can ignore it
+input wire clk, // clock
+input wire rst_n // reset_n - low to reset
 );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+wire A = ui_in[0];
+wire B = ui_in[1];
+wire Yd;
+wire Y;
+// Logic implementation
+and(Yd, A, B);
+not(Y, Yd);
+9
+// Assign outputs
+assign uo_out[0] = Y;
+assign uo_out[1] = 1'b0;
+assign uo_out[2] = 1'b0;
+assign uo_out[3] = 1'b0;
+assign uo_out[4] = 1'b0;
+assign uo_out[5] = 1'b0;
+assign uo_out[6] = 1'b0;
+assign uo_out[7] = 1'b0;
+assign uio_out = 0;
+assign uio_oe = 0;
+// List all unused inputs to prevent warnings
+wire _unused = &{ena, clk, rst_n, ui_in[7:2], uio_in, 1'b0};
 endmodule
